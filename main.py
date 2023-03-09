8058f278f0bb4f2473dd507313fb7dd1ea109f02
Ramen Noodles <115237369+Whevat@users.noreply.github.com>
2023-03-09 15:53:26 +0000

    Added ... if the user responds with something non-acceptable and started the combat system.

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 import time
 import os
+import random
 
+#Map Variables
 x = 1
 o = "o"
 _ = '_'
 left = 5
 right = 5
 
+Battle_Chance = 1
+
+#Player Stats
+Phealth = 100
+Pattack = 5
+Pdefense = 0
+
+#Enemy Stats
+Ehealth = 50
+Eattack = 5
+Edefense = 5
+
+
 print(map)
+
+def Battle():
+	print("An Enemy Approaches!")
+
+def other_input():
+	p = '.'
+	for x in range(4):
+		print(x * p, end='\r')
+		time.sleep(.5)
 
 while x == 1: 
 	map = (left * _ + o + _ * right)
@@ -32,3 +56,6 @@ while x == 1:
 			print(map)
 		elif left == 0:
 			print("Dead End")
+	if move != "right" or "left":
+		other_input()
+		
